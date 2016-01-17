548f33b4eabb9c41e96c4ea2c3fcdadd814b0a78
radostin-angelov <angelovradostin@gmail.com>
2016-01-17 03:41:13 +0200

    id fix

--- Client/WoahMe/app/src/main/java/woahme/teamwork/com/woahme/PlacesListAdapter.java
+++ Client/WoahMe/app/src/main/java/woahme/teamwork/com/woahme/PlacesListAdapter.java
@@ -1,14 +1,17 @@
 package woahme.teamwork.com.woahme;
 
 import android.content.Context;
+import android.net.Network;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import com.android.volley.toolbox.NetworkImageView;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 import woahme.teamwork.com.woahme.Http.SingletonRequestQueue;
@@ -40,9 +43,13 @@ public class PlacesListAdapter extends ArrayAdapter<PlaceModel> {
 
             ViewHolder viewHolder = new ViewHolder();
 
-            viewHolder.image = (NetworkImageView) view.findViewById(R.id.image_view);
+            viewHolder.image = (NetworkImageView) view.findViewById(R.id.imageSource);
             view.setTag(viewHolder);
         }
+
+        PlaceModel currentItem = items.get(position);
+        NetworkImageView image = (NetworkImageView) view.findViewById(R.id.imageSource);
+        image.setImageUrl(currentItem.getImageSource(), SingletonRequestQueue.getInstance(this.context).getImageLoader());
 
         ViewHolder viewHolder = (ViewHolder) view.getTag();
         viewHolder.id = items.get(position).getId();
