28562e876d444f615265b2ccfe8d23916525079b
Nicky <newmast@abv.bg>
2016-01-18 02:05:35 +0300

    added final details page

--- Client/WoahMe/app/src/main/java/woahme/teamwork/com/woahme/DetailsActivity.java
+++ Client/WoahMe/app/src/main/java/woahme/teamwork/com/woahme/DetailsActivity.java
@@ -1,18 +1,14 @@
 package woahme.teamwork.com.woahme;
 
-import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import com.android.volley.toolbox.NetworkImageView;
-
-import woahme.teamwork.com.woahme.Http.LruBitmapCache;
 import woahme.teamwork.com.woahme.Http.SingletonRequestQueue;
 
 public class DetailsActivity extends AppCompatActivity {
     TextView titleView;
-    ZoomableImageView imageView;
+    NetworkImageView imageView;
     TextView descriptionView;
     TextView creatorView;
     TextView locationName;
@@ -23,7 +19,7 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         this.titleView = (TextView) findViewById(R.id.place_details_title);
-        this.imageView = (ZoomableImageView) findViewById(R.id.place_details_image);
+        this.imageView = (NetworkImageView) findViewById(R.id.place_details_image);
         this.descriptionView = (TextView) findViewById(R.id.place_details_description);
         this.creatorView = (TextView) findViewById(R.id.place_details_creator);
         this.locationName = (TextView) findViewById(R.id.place_details_location_name);
